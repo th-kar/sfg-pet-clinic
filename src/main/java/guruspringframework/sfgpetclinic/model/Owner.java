2ff3f12f2f9810f8414d40822cd15a010601bc87
Thanos Karagiannis <than.karagiannis@gmail.com>
2018-09-12 10:16:10 +0300

    Adding POJO. Closing issue #2

---
+++ src/main/java/guruspringframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guruspringframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guruspringframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package guruspringframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
